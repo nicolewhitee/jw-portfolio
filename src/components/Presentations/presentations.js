23a7f300c9a3a4f1cf88f1e8760972389ffd5f7d
Nicole White <nicolemwhite4@gmail.com>
2023-12-30 14:42:21 -0500

    Remove unused include

--- src/components/Presentations/presentations.js
+++ src/components/Presentations/presentations.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './presentations.css';
 import presentationsArray from './presentationsArray.js';
 
