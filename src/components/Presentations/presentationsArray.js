f4a1b82ed0954b4a0d1d5d7b21603135554d9f53
Nicole White <nicolemwhite4@gmail.com>
2024-03-01 12:09:48 -0500

    Update presentations array

--- src/components/Presentations/presentationsArray.js
+++ src/components/Presentations/presentationsArray.js
@@ -1,123 +1,115 @@
 const presentationsArray = [
     {
+        event: 'Dell Technologies',
+        location: 'Online',
+        title: 'Inferencing at the Edge',
+        date: 'Feb 28, 2024',
+        link: 'https://infohub.delltechnologies.com/en-US/p/inferencing-at-the-edge/'
+    },
+    {
         event: 'Precision Convergence Webinar',
         location: 'Online McGill University & CMU',
         title: 'Evolution of the Intelligent Digital Twin',
         date: 'Dec 14, 2023',
-        link: 'https://www.mcgill.ca/desautels/mcche/events/webinar-series/precision-convergence-webinar-series',
-        imageLink: ''
+        link: 'https://www.mcgill.ca/desautels/mcche/events/webinar-series/precision-convergence-webinar-series'
     },
     {
         event: 'IEEE / ACM Symposium on Edge Compute',
         location: 'Wilmington, DE',
         title: 'Edgenuity: Technical Challenges and Opportunities for Modern Edge Computation',
         date: 'Dec 6, 2023',
-        link: 'https://acm-ieee-sec.org/2023/',
-        imageLink: ''
+        link: 'https://acm-ieee-sec.org/2023/'
     },
     {
         event: 'Digital Twin Consortium',
         location: 'Austin, TX',
         title: 'Advanced Model Predictive Control Using AI and Digital Twin for Distributed System Platforms',
         date: 'Dec 5, 2023',
-        link: 'https://www.digitaltwinconsortium.org/events/q4-member-meeting-2023/',
-        imageLink: ''
+        link: 'https://www.digitaltwinconsortium.org/events/q4-member-meeting-2023/'
     },
     {
         event: 'GE Global Research – Edge Symposium',
         location: 'Niskayuna, NY',
         title: 'How AI/ML is Used to Build the Modern Software Defined Edge Operating Platform',
         date: 'Sep 19, 2023',
-        link: 'https://www.ge.com/research/newsroom/inventing-future-latest-advances-edge-5g-controls-cybersecurity-ai-and-robotics',
-        imageLink: ''
+        link: 'https://www.ge.com/research/newsroom/inventing-future-latest-advances-edge-5g-controls-cybersecurity-ai-and-robotics'
     },
     {
         event: 'AI Hardware and Edge AI Summit',
         location: 'Santa Clara, CA',
         title: 'Edge Inference @ Scale, Harder than it Looks',
         date: 'Sep 14, 2023',
-        link: 'https://aihwedgesummit.com/ai-hardware-edge-ai-summit-2023/ai-hardware-edge-ai-summit-2023-agenda',
-        imageLink: ''
+        link: 'https://aihwedgesummit.com/ai-hardware-edge-ai-summit-2023/ai-hardware-edge-ai-summit-2023-agenda'
     },
     {
         event: 'Dell Technologies Forum',
         location: 'Tel Aviv',
         title: 'Dell Technology Forum – Dell Edge Strategy & Execution',
         date: 'Sep 12, 2023',
-        link: 'https://www.dell.com/en-us/dt/events/forums.htm#/',
-        imageLink: ''
+        link: 'https://www.dell.com/en-us/dt/events/forums.htm#/'
     },
     {
         event: 'Texas Digital Government Summit',
         location: 'Austin, TX',
         title: 'Is It Real or Is It AI',
         date: 'Jun 29, 2023',
-        link: 'https://dir.texas.gov/events/texas-digital-government-summit-2023',
-        imageLink: ''
+        link: 'https://dir.texas.gov/events/texas-digital-government-summit-2023'
     },
     {
         event: 'Digital Twin Consortium',
         location: 'Orlando, FL',
         title: 'Modeling Dynamics in Industrial Systems Using Computer Vision with Point Clouds',
         date: 'Jun 20, 2023',
-        link: 'https://www.digitaltwinconsortium.org/events/dtc-q2-member-meeting-2024/',
-        imageLink: ''
+        link: 'https://www.digitaltwinconsortium.org/events/dtc-q2-member-meeting-2024/'
     },
     {
         event: 'Intelligent Traffic Systems',
         location: 'Dallas, TX',
         title: 'Edge in collection and operation of traffic flow',
         date: 'Apr 24, 2023',
-        link: 'https://itsa.org/event/its-america-conference-expo-2023-dallas/',
-        imageLink: ''
+        link: 'https://itsa.org/event/its-america-conference-expo-2023-dallas/'
     },
     {
         event: 'Antheneum',
         location: 'Online',
         title: 'Edge Technical Drivers & Challenges - Hyperautomation',
         date: 'Mar 29, 2023',
-        link: 'https://www.atheneum.ai/webinars',
-        imageLink: ''
+        link: 'https://www.atheneum.ai/webinars'
     },
     {
         event: 'NIST - NDN Communications Summit',
         location: 'Online',
         title: 'Edge Information Management Demand is Only Growing',
         date: 'Mar 2, 2023',
-        link: 'https://www.nist.gov/news-events/events/2023/03/ndncomm-2023',
-        imageLink: ''
+        link: 'https://www.nist.gov/news-events/events/2023/03/ndncomm-2023'
     },
     {
         event: 'Digital Twin Consortium',
         location: 'Online',
         title: 'Edgenuity – How Will Edge and Digital Twins Intersect',
         date: 'Feb 22, 2023',
-        link: 'https://www.digitaltwinconsortium.org/events/the-value-of-digital-twins-from-concept-to-reality/',
-        imageLink: ''
+        link: 'https://www.digitaltwinconsortium.org/events/the-value-of-digital-twins-from-concept-to-reality/'
     },
     {
         event: 'IOT Solution World Congress',
         location: 'Barcelona, Spain',
         title: 'Vision and Industry Initiatives Driving the Future of Mobility',
         date: 'Jan 31, 2023',
-        link: 'https://www.iiconsortium.org/events/iotswc-2023/',
-        imageLink: ''
+        link: 'https://www.iiconsortium.org/events/iotswc-2023/'
     },
     {
         event: 'Emerson Exchange Summit',
         location: 'Dallas, TX',
         title: 'Edge Impact of Sustainability',
         date: 'Oct 25, 2022',
-        link: 'https://www.emerson.com/en-us/automation/events/emerson-exchange/americas',
-        imageLink: ''
+        link: 'https://www.emerson.com/en-us/automation/events/emerson-exchange/americas'
     },
     {
         event: 'GE Global Research – Edge Symposium',
         location: 'Niskayuna, NY',
         title: 'Edgenuity: Drivers, Innovation, & Technology',
         date: 'Sep 19, 2022',
-        link: 'https://www.youtube.com/playlist?list=PL7cXTVV5-YmKKAqTPUHFr3_wucrTMNVkF',
-        imageLink: ''
+        link: 'https://www.youtube.com/playlist?list=PL7cXTVV5-YmKKAqTPUHFr3_wucrTMNVkF'
     }
 ]
 
