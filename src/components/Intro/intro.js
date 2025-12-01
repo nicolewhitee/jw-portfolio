c6cd6229c6abf43c6bac3261c476f436bdadf251
Nicole White <nicolemwhite4@gmail.com>
2025-11-30 22:12:35 -0500

    Update title

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -46,8 +46,8 @@ const Intro = () => {
             <div className="introContent">
                 <div className="revealText">
                     <div className="introName" aria-level="1" role="heading">Jeff White</div>
-                    <div className="titleText" aria-level="2" role="heading">AI Factory and AI Edge Product Lead</div>
-                    <div className="titleText" aria-level="2" role="heading">at Dell Technologies</div>
+                    <div className="titleText" aria-level="2" role="heading">Chief Architect for Office of CTO <br />and Managing Director</div>
+                    <div className="titleText" aria-level="2" role="heading">at Ernst and Young</div>
                     <div className="introPara">Experienced technology executive with proven record of accomplishment</div>
                 </div>
                 <button className="yellowBtn" onClick={() => {
